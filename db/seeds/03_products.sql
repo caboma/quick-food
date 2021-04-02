96d7495a60e5d8298a4bfd449d37b4869f3d9e8e
Zac Alexander <zac@znacreative.com>
2021-04-02 23:47:23 +0000

    Add product seeds to new DB

--- db/seeds/03_products.sql
+++ db/seeds/03_products.sql
@@ -1,3 +1,15 @@
 INSERT INTO products(name, description, time_to_ready, restaurant_id, price_cents) VALUES ('BigMac','Delicious Burguer', 30, 1,800);
 INSERT INTO products(name, description, time_to_ready, restaurant_id, price_cents) VALUES ('Wooper', 'Fresh Burguer', 20, 2,700);
-INSERT INTO products(name, description, time_to_ready, restaurant_id, price_cents) VALUES ('Chicken Sandwich','Crispy Chicken', 40,3,600);
+INSERT INTO products(name, description, time_to_ready, restaurant_id, price_cents)
+VALUES ('Chicken Sandwich','Crispy Chicken', 40,3,600),
+('Cheese Burger', 'Hot and cheesy', 30, 1, 800),
+('Peperoni Pizza', 'Baked in our wood burning oven!', 500, 1, 1500),
+('Poutine', 'Canadian Goodness', 50, 1, 750),
+('Taco', 'Delicious!', 50, 1, 2000),
+('Poke', 'One of a kind!', 50, 1, 1699),
+('Hot dog', 'Ballpark style', 20, 1, 500),
+('Burrito', 'Like no where else!', 20, 1, 900),
+('Coke', 'Better than Pepsi!', 0, 1, 150),
+('Orange Juice', 'from concentrate', 0, 1, 200),
+('Sprite', 'Lemon fresh', 0, 1, 150),
+('Canada Dry', 'Famous ginger ale', 0, 1, 150);
