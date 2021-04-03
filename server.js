5eb5e32b6ab2acaf684226bb095fcf20f14b7637
Zac Alexander <zac@znacreative.com>
2021-04-03 14:54:31 +0000

    Add Twilio config section on line 13 to line 21 to receive sms

--- server.js
+++ server.js
@@ -10,6 +10,16 @@ const sass       = require("node-sass-middleware");
 const app        = express();
 const morgan     = require('morgan');
 
+//Twilio config
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
+
+app.post('/sms', (req, res) => {
+  const twiml = new MessagingResponse();
+  twiml.message('The Robots are coming! Head for the hills!');
+  res.writeHead(200, {'Content-Type': 'text/xml'});
+  res.end(twiml.toString());
+});
+
 // PG database client/connection setup
 const { Pool } = require('pg');
 const dbParams = require('./lib/db.js');
