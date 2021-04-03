cb319fa65861a1c92d0231d0a0ad2bb2d314968f
sandypockets <zac@znacreative.com>
2021-04-03 14:22:31 -0400

    Merge branch 'master' of github.com:sandypockets/quick-food into feature-twilio

---
+++ public/scripts/send_sms.js
@@ -0,0 +1,35 @@
+// Download the helper library from https://www.twilio.com/docs/node/install
+// Your Account Sid and Auth Token from twilio.com/console
+// and set the environment variables. See http://twil.io/secure
+require('dotenv').config();
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+
+// Initial test message
+const message = 'Your order will be ready for pick up in 5 minutes!';
+const phone = '+16137905701';
+
+const sendSms = (phone, message) => {
+  client.messages
+    .create({
+       body: message,
+       from: '+13658000804',
+       to: phone
+     })
+    .then(message => console.log(message.sid, message.status))
+    .catch(err => console.error(err));
+};
+// sendSms(phone, message);
+
+// jQuery button/sms event handler
+$(document).ready(function () {
+  // Send sms to restaurant when client places order
+  $('#client-place-order').on('click', function() {
+    sendSms(phone, message);
+  })
+  // Send sms to client when restaurant confirms order
+  $('#restaurant-confirm-order').on('click', function() {
+    sendSms(phone, message);
+  })
+});
--- routes/index.js
+++ routes/index.js
@@ -6,21 +6,21 @@
  */
 
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
-    let query = `SELECT * FROM widgets`;
+    let query = `SELECT * FROM products`;
     console.log(query);
     db.query(query)
       .then(data => {
-        const widgets = data.rows;
-        res.json({ widgets });
+        const products = data.rows;
+        res.render('../views/index');
       })
       .catch(err => {
         res
           .status(500)
-          .json({ error: err.message });
+          .json({error: err.message});
       });
   });
   return router;
