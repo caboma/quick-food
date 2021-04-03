03fe37dba096a37f9f40baac9f6e7c92176cc50d
Zac Alexander <zac@znacreative.com>
2021-04-03 14:53:56 +0000

    Test SMS sends properly

--- send_sms.js
+++ send_sms.js
@@ -1,8 +1,10 @@
 // Download the helper library from https://www.twilio.com/docs/node/install
 // Your Account Sid and Auth Token from twilio.com/console
 // and set the environment variables. See http://twil.io/secure
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
+const accountSid = 'AC950a742374dfdad1b0338b06dced0ef6';
+const authToken = '56b4f369542a4226ed6635c123bd3137';
+// const accountSid = process.env.TWILIO_ACCOUNT_SID;
+// const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
 
 client.messages
