5a51e5a79656f4d690c84c0f71e42cf08c932dc1
Gabriel Silva <j.gabriel.888@gmail.com>
2021-04-03 00:29:40 +0000

    Seeds file and schema changed, category row added

--- db/schema/03_products.sql
+++ db/schema/03_products.sql
@@ -3,6 +3,7 @@ CREATE TABLE products (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
   description VARCHAR(255) NOT NULL,
+  category VARCHAR (255) NOT NULL,
   time_to_ready INTEGER NOT NULL,
   restaurant_id INTEGER REFERENCES restaurants(id) ON DELETE CASCADE,
   price_cents INTEGER NOT NULL
