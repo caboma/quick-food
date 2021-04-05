9ace7b805bc33e8b8d14548d1acf771c82ddae42
Zac A <41454557+sandypockets@users.noreply.github.com>
2021-04-05 18:34:07 -0400

    Pasting from local machine

--- routes/login.js
+++ routes/login.js
@@ -11,3 +11,27 @@ module.exports = (db) => {
   })
   return router;
 };
+
+// From before
+/* const express = require('express');
+const router = express.Router();
+
+module.exports = (db) => {
+  router.get("/login", (req, res) => {
+    console.log("***********************")
+    let queryString = `SELECT * FROM users`;
+    db.query(queryString)
+      .then(data => {
+        users = data.rows;
+        const templateVars = {product: productLists};
+        res.send("LOGIN");
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({error: err.message});
+      });
+  });
+  return router;
+};
+ */
