7690c5a1fce9dcd35aed4503edf98b0f8a3beb2b
Omar Cabatbat <omar_cabatbat@yahoo.com>
2021-04-05 13:36:24 +0000

    Fix .jpeg to .jpg issue

--- db/seeds/03_products.sql
+++ db/seeds/03_products.sql
@@ -1,16 +1,16 @@
 INSERT INTO products(name, description,category, time_to_ready, restaurant_id, price_cents, image)
 VALUES
-('Cheese Burguer','Angus Beef, Letuce, Tomato, Cheese','food', 20, 1, 600, 'images/01_CheeseBurger.jpeg'),
-('Cheese Burguer Bacon', 'Angus Beef, Letuce, Tomato, Cheese, Bacon','food', 20, 1, 800,'images/02_CheeseBurger_Bacon.jpeg'),
-('Chicken Sandwich','Crispy Chicken, Letuce, Tomato','food', 40, 1, 800,'images/03_chickenSandwich.jpeg'),
-('Cheese Burger Combo', 'Angus Beef, Letuce, Tomato, Cheese, Fries, Drinks','meal', 30, 1, 1200, 'images/Combo_CheeseBurger.jpeg'),
-('Cheese Burger Bacon Combo', 'Angus Beef, Letuce, Tomato, Cheese, Bacon, Fries, Drinks','meal', 30, 1, 1300, 'images/Combo_CheeseBurger_Bacon.jpeg'),
-('Chicken Sandwich Combo','Crispy Chicken, Letuce, Tomato, Fries, Drinks','meal', 40, 1, 1400, 'images/Combo_ChickenSandwich.jpeg'),
+('Cheese Burguer','Angus Beef, Letuce, Tomato, Cheese','food', 20, 1, 600, 'images/01_CheeseBurger.jpg'),
+('Cheese Burguer Bacon', 'Angus Beef, Letuce, Tomato, Cheese, Bacon','food', 20, 1, 800,'images/02_CheeseBurger_Bacon.jpg'),
+('Chicken Sandwich','Crispy Chicken, Letuce, Tomato','food', 40, 1, 800,'images/03_chickenSandwich.jpg'),
+('Cheese Burger Combo', 'Angus Beef, Letuce, Tomato, Cheese, Fries, Drinks','meal', 30, 1, 1200, 'images/Combo_CheeseBurger.jpg'),
+('Cheese Burger Bacon Combo', 'Angus Beef, Letuce, Tomato, Cheese, Bacon, Fries, Drinks','meal', 30, 1, 1300, 'images/Combo_CheeseBurger_Bacon.jpg'),
+('Chicken Sandwich Combo','Crispy Chicken, Letuce, Tomato, Fries, Drinks','meal', 40, 1, 1400, 'images/Combo_ChickenSandwich.jpg'),
 ('Fries', 'Flavourful Fries','food', 10, 1, 300, 'images/french_fries.png'),
-('Coke', 'Better than Pepsi!','drink', 0, 1, 150, 'images/coke.jpeg'),
+('Coke', 'Better than Pepsi!','drink', 0, 1, 150, 'images/coke.jpg'),
 ('Orange Juice', 'from concentrate','drink', 0, 1, 200, 'images/OrangeJuice.png'),
-('Sprite', 'Lemon fresh', 'drink',0, 1, 150, 'images/sprite.jpeg'),
-('Canada Dry', 'Famous ginger ale','drink', 0, 1, 150, 'images/canadaDry.jpeg');
+('Sprite', 'Lemon fresh', 'drink',0, 1, 150, 'images/sprite.jpg'),
+('Canada Dry', 'Famous ginger ale','drink', 0, 1, 150, 'images/canadaDry.jpg');
 
 
 -- ('Peperoni Pizza', 'Baked in our wood burning oven!','food', 500, 1, 1500),
