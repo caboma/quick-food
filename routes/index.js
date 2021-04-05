6b36ec7f39f51cdcd3dc6aab86d6924c17282387
Omar Cabatbat <omar_cabatbat@yahoo.com>
2021-04-05 20:03:38 +0000

    Created restaurant route

--- routes/index.js
+++ routes/index.js
@@ -24,5 +24,57 @@ module.exports = (db) => {
           .json({error: err.message});
       });
   });
+
+  router.get("/restaurant", (req, res) => {
+    let queryString = `
+      SELECT orders.id AS order_number, users.name AS customer, orders.total_amount AS total, orders.status AS status
+      FROM orders JOIN users ON orders.user_id = users.id ORDER BY orders.id`;
+    db.query(queryString)
+      .then(data => {
+        orderLists = data.rows;
+        const templateVars = {orders: orderLists};
+        res.render("restaurants", templateVars);
+
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({error: err.message});
+      });
+  })
+
+  // router.get("/restaurant/:order_id", (req, res) => {
+  //   let queryString = `
+  //   SELECT order_details.order_id, products.name, products.price_cents FROM order_details JOIN products ON order_details.product_id = products.id WHERE order_details.order_id = '1'`;
+  //   db.query(queryString)
+  //     .then(data => {
+  //       orderLists = data.rows;
+  //       const templateVars = {orders: orderLists};
+  //       res.render("restaurants", templateVars);
+  //     })
+  //     .catch(err => {
+  //       res
+  //         .status(500)
+  //         .json({error: err.message});
+  //     });
+  // })
+
+
+  router.post("/restaurant", (req, res) => {
+    const order_id = req.body.orderID;
+    const order_status = req.body.status;
+    console.log(req.body)
+
+    let queryString = `UPDATE orders SET status = '${order_status}' WHERE id = '${order_id}'`;
+    db.query(queryString)
+      .then(data => {
+        res.redirect('/restaurant');
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({error: err.message});
+      });
+  })
   return router;
 };
