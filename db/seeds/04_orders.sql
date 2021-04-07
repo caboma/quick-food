8b82fd46b612e723f81b81a0cbdbea9f24984d52
Gabriel Silva <j.gabriel.888@gmail.com>
2021-04-07 19:12:51 +0000

    /orders route created with query to retrive order list from userid

--- db/seeds/04_orders.sql
+++ db/seeds/04_orders.sql
@@ -1,7 +1,7 @@
-INSERT INTO orders(id, user_id, restaurant_id, total_amount, status)
+INSERT INTO orders(id, user_id, restaurant_id, status)
 VALUES
-(1, 1, 1, 4500, 'Ready'),
-(2, 1, 1, 2500, 'Ready'),
-(3, 1, 1, 6500, 'Ready'),
-(4, 2, 1, 3000, 'Ready'),
-(5, 3, 1, 4500, 'Ready');
+(1, 1, 1, 'Ready'),
+(2, 1, 1, 'Ready'),
+(3, 1, 1, 'Ready'),
+(4, 2, 1, 'Ready'),
+(5, 3, 1, 'Ready');
