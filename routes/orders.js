4a9caacabb5bcd42c97b87de3f70bde663a9007d
Omar Cabatbat <omar_cabatbat@yahoo.com>
2021-04-08 18:55:18 +0000

    Fixed my order query

--- routes/orders.js
+++ routes/orders.js
@@ -7,32 +7,25 @@ const dbParams = require('../lib/db');
 module.exports = (db) => {
   router.get("/", (req, res) => {
     const userID = req.session.user_id;
-    let queryString = `
-    SELECT * FROM orders WHERE user_id = ${userID} ORDER BY id DESC`;
+    let queryString = ` SELECT orders.*, products.name FROM orders JOIN order_details ON order_details.order_id = orders.id
+    JOIN products ON products.id = order_details.product_id
+    WHERE orders.user_id = ${userID} ORDER BY orders.id DESC`;
     db.query(queryString)
-      .then(data => {
-        let ordersByUser = data.rows;
-        const nameList = {};
-        for (let orderNo of ordersByUser) {
-          let queryProductName = `
-            SELECT products.name FROM order_details
-            JOIN products ON products.id = order_details.product_id WHERE order_id = ${orderNo.id}
-          `;
-          db.query(queryProductName)
-            .then(data => {
-              let itemLists = data.rows;
-              nameList[orderNo.id] = itemLists;
-              if (orderNo.id === ordersByUser[ordersByUser.length - 1].id) {
-                let templateVars = {orders: ordersByUser, user: userID, productNames: nameList};
-                res.render("my-orders", templateVars);
-              }
-            });
+    .then (data => {
+      const orders = {};
+      const nameList = [];
+      for (let row of data.rows) {
+        if (!orders[row.id]) {
+          orders[row.id] = {status: '', productNames: []};
         }
-      })
-      .catch(err => {
-        res.status(500)
-          .json({error: err.message});
-      });
+        orders[row.id].productNames.push(row.name);
+        console.log(row.name);
+        orders[row.id].status = row.status;
+      }
+      const templateVars = {user: userID, orders: orders}
+      console.log(templateVars);
+      res.render("my-orders", templateVars);
+    })
   });
   return router;
 };
