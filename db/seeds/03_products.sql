1f613630ae888735be269aa3adddb0a48d22a7b3
Omar Cabatbat <omar_cabatbat@yahoo.com>
2021-04-09 12:59:52 +0000

    Fix typo error in product seeds

--- db/seeds/03_products.sql
+++ db/seeds/03_products.sql
@@ -1,7 +1,7 @@
 INSERT INTO products(name, description,category, time_to_ready, restaurant_id, price_cents, image)
 VALUES
-('Cheese Burguer','Angus Beef, Letuce, Tomato, Cheese','food', 20, 1, 600, 'images/01_CheeseBurger.jpg'),
-('Cheese Burguer Bacon', 'Angus Beef, Letuce, Tomato, Cheese, Bacon','food', 20, 1, 800,'images/02_CheeseBurger_Bacon.jpg'),
+('Cheese Burger','Angus Beef, Letuce, Tomato, Cheese','food', 20, 1, 600, 'images/01_CheeseBurger.jpg'),
+('Cheese Burger Bacon', 'Angus Beef, Letuce, Tomato, Cheese, Bacon','food', 20, 1, 800,'images/02_CheeseBurger_Bacon.jpg'),
 ('Chicken Sandwich','Crispy Chicken, Letuce, Tomato','food', 40, 1, 800,'images/03_chickenSandwich.jpg'),
 ('Cheese Burger Combo', 'Angus Beef, Letuce, Tomato, Cheese, Fries, Drinks','meal', 30, 1, 1200, 'images/Combo_CheeseBurger.jpg'),
 ('Cheese Burger Bacon Combo', 'Angus Beef, Letuce, Tomato, Cheese, Bacon, Fries, Drinks','meal', 30, 1, 1300, 'images/Combo_CheeseBurger_Bacon.jpg'),
@@ -11,22 +11,3 @@ VALUES
 ('Orange Juice', 'from concentrate','drink', 0, 1, 200, 'images/OrangeJuice.png'),
 ('Sprite', 'Lemon fresh', 'drink',0, 1, 150, 'images/sprite.jpg'),
 ('Canada Dry', 'Famous ginger ale','drink', 0, 1, 150, 'images/canadaDry.jpg');
-
-
--- ('Peperoni Pizza', 'Baked in our wood burning oven!','food', 500, 1, 1500),
--- ('Poutine', 'Canadian Goodness','food', 50, 1, 750),
--- ('Taco', 'Delicious!','food', 50, 1, 2000),
--- ('Poke', 'One of a kind!','food', 50, 1, 1699),
--- ('Hot dog', 'Ballpark style','food', 20, 1, 500),
--- ('Burrito', 'Like no where else!','food', 20, 1, 900),
--- ('Nachos', 'The real mexican!','food', 40, 1, 850),
--- ('Fish n Chips', 'Fresh ingredients','food', 50, 1, 1600),
-
--- ('Chocolate Cookie','Chunks of chocolate!','desert',15,1 ,250),
--- ('Chocolate Muffin','Deliciously soft','desert',20,1 ,350),
--- ('Chocolate Mousse','Creamy dark chocolate','desert',30,1 ,450),
--- ('Carrot Cake','Home made','desert',20,1 ,350),
--- ('BurguerTine Combo','Cheeseburguer and poutine','combo',45,1 ,1300),
--- ('BurguerTine Drink','Cheeseburguer, poutine and drink','combo',40,1 ,1450),
--- ('All in One','Cheeseburguer, drink and desert','combo',50,1 ,1600),
--- ('Family Combo','4 Burguers, 4 Drinks','combo',60,1 ,3500);
